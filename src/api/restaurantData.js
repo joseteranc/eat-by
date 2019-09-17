b99c4830ba1a4f803bfa844367f75c69ca7b04fd
jose teran <joseantonioterancevallos@gmail.com>
2019-09-17 17:40:04 +0200

    add funtional filters

--- src/api/restaurantData.js
+++ src/api/restaurantData.js
@@ -3,37 +3,41 @@ const restaurants = [
     imgSrc:
       "https://d2p4qauh5fa1j0.cloudfront.net/fotos/recetas/ampliacion/cevic_IEpBSWZbvdOc7m59xHYX4tVfMDrunK.jpg",
     title: "El Ceviche",
-    categories: ["ecuadorian"],
-    distance: 1,
+    category: ["ecuadorian", "street food", "see food", "wrap"],
+    distance: 5,
     rating: 4.5,
     votes: 5,
+    price: 8,
     description: "excelent"
   },
   {
     imgSrc: "",
     title: "currySelector",
-    categories: ["indian"],
-    distance: 2,
+    category: ["indian", "pasta", "see food", "wrap"],
+    distance: 8,
     rating: 3.7,
     votes: 10,
+    price: 4,
     description: "long wait"
   },
   {
     imgSrc: "",
     title: "la cucina di ",
-    categories: ["italian"],
-    distance: 2,
+    category: ["italian", "pasta", "see food", "wrap"],
+    distance: 10,
     rating: 3.7,
-    votes: 10,
+    votes: 14,
+    price: 9,
     description: "long wait"
   },
   {
     imgSrc: "",
     title: "Wurst Kultur",
-    categories: ["deutsch"],
-    distance: 2,
+    category: ["deutsch", "pasta", "street food", "wrap"],
+    distance: 3,
     rating: 3.7,
     votes: 10,
+    price: 7,
     description: "long wait"
   }
 ];
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,15 +2,57 @@ import React from "react";
 import Restaurant from "./Restaurant";
 import restaurants from "../api/restaurantData";
 
-function RestaurantList() {
+function RestaurantList({ selectedFilters }) {
+  console.log(selectedFilters);
+  const filteredRestaurants = restaurants.filter(restaurant => {
+    let keepRestaurant = true;
+    if (!selectedFilters) {
+      return true;
+    }
+    if (selectedFilters.distance) {
+      switch (selectedFilters.distance) {
+        case "<2min":
+          keepRestaurant = restaurant.distance < 2;
+          break;
+        case "<5min":
+          keepRestaurant = restaurant.distance < 5;
+          break;
+        case "<10min":
+          keepRestaurant = restaurant.distance <= 10;
+          break;
+        default:
+          break;
+      }
+    }
+    if (keepRestaurant && selectedFilters.category) {
+      keepRestaurant = restaurant.category.includes(selectedFilters.category);
+    }
+    if (selectedFilters.price) {
+      switch (selectedFilters.price) {
+        case "<3":
+          keepRestaurant = restaurant.price < 3;
+          break;
+        case "<5":
+          keepRestaurant = restaurant.distance < 5;
+          break;
+        case "<10":
+          keepRestaurant = restaurant.distance < 10;
+          break;
+        default:
+          break;
+      }
+    }
+    return keepRestaurant;
+  });
   return (
     <section className="restaurantlist">
-      {restaurants.map(restaurant => {
+      {filteredRestaurants.map(restaurant => {
         return <Restaurant key={restaurant.title} restaurant={restaurant} />;
       })}
     </section>
   );
 }
+export default RestaurantList;
 
 // function RestaurantList() {
 //   return (
@@ -24,5 +66,3 @@ function RestaurantList() {
 //     </section>
 //   );
 // }
-
-export default RestaurantList;
